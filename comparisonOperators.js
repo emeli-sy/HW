b380c6642d565a61b4daff2b620ed3e2f3040de9
emeli-sy <emeli.yakimova@gmail.com>
2024-07-18 18:56:06 +0300

    operators

---
+++ comparisonOperators.js
@@ -0,0 +1,14 @@
+// console.log( 5 == 5 );
+// console.log( 5 === 5 )
+// console.log( 5 === "5" );
+// console.log( 5 == "5" );
+// console.log( "5" == "5" );
+// console.log( "5" === "5");
+
+
+console.log( true == 1 );
+console.log( true == 0 );
+console.log( false == 0 );
+console.log( true > false );
+
+
