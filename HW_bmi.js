d79d69102ccc871ef9a595819c8f213f8d1eb797
emeli-sy <emeli.yakimova@gmail.com>
2024-07-18 18:25:13 +0300

    BMI calculation

---
+++ HW_bmi.js
@@ -0,0 +1,10 @@
+var bodyWeight;
+var height;
+var bodyMassIndex;
+
+bodyWeight = "60";
+height = " 1.65 ";
+bodyMassIndex = bodyWeight / height**2;
+
+console.log( "bodyMassIndex", bodyMassIndex );
+
--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log(1);
+// console.log(1);
+
+
+// for (;;){
+//     console.log('Hi');
+// }
+
+
+
+// var firstName;
+// var lastName;
+// var fullName;
+
+// firstName = "Ada";
+// lastName = "Byron";
+// fullName = firstName + " " + lastName;
+
+// console.log( "fullName", fullName );
+
+
+var addressCity;
+var addressStreet;
+var addressZipCode;
+var fullAddress;
+
+addressCity = "Sofia";
+addressStreet = "Madara";
+addressZipCode = "1000";
+fullAddress = addressCity + " " + addressStreet + " " + addressZipCode;
+
+console.log( "fullAddress", fullAddress );
+
+
+
+
+
+
+
